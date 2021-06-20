a2709c524c2c6ba71eb23304499413d0ab84f3f8
Felipe Martins <felipemartoliveira@gmail.com>
2021-06-20 15:09:15 -0300

    Initial Commit

---
+++ Laravel-Ediaristas/resources/views/create.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('titulo', 'Criar diarista')
+
+@section('conteudo')
+<h1>Criar diarista</h1>
+<form action="{{ route('diaristas.store') }}" method="POST" enctype="multipart/form-data">
+    @include('_form')
+</form>
+@endsection
